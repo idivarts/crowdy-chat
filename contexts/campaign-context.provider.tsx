d13771031d094343f8f0f61631160bbf6e87a95b
dev-rathore <rathoredvd3001@gmail.com>
2024-08-29 21:04:55 +0530

    fetch campaigns for orgId

--- contexts/campaign-context.provider.tsx
+++ contexts/campaign-context.provider.tsx
@@ -1,13 +1,15 @@
 import { initialCampaigns } from "@/constants/Campaigns";
 import { FirestoreDB } from "@/shared-libs/utilities/firestore";
 import { Campaign } from "@/types/campaign";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import {
   useContext,
   createContext,
   type PropsWithChildren,
   useState,
+  useEffect,
 } from "react";
+import { useOrganizationContext } from "./organization-context.provider";
 
 interface CampaignContextProps {
   campaigns: Campaign[];
@@ -24,12 +26,25 @@ export const CampaignContextProvider: React.FC<PropsWithChildren> = ({
   console.log(initialCampaigns);
   const [campaigns, setCampaigns] = useState<Campaign[]>([]);
 
+  const {
+    currentOrganization,
+  } = useOrganizationContext();
+
+  const fetchCampaigns = async () => {
+    await getCampaigns();
+  }
+
+  useEffect(() => {
+    fetchCampaigns();
+  }, [currentOrganization]);
+
   const getCampaigns = async () => {
     // Fetch data from Firebase
-    const campaignsRef = collection(FirestoreDB, "organizations");
-    const campaignsDoc = await getDocs(campaignsRef);
+    const campaignsRef = collection(FirestoreDB, "campaigns");
+    const campaignsQuery = query(campaignsRef, where("organizationId", "==", currentOrganization?.id));
+    const campaignsSnapshot = await getDocs(campaignsQuery);
 
-    const campaignsData = campaignsDoc.docs.map((doc) => {
+    const campaignsData = campaignsSnapshot.docs.map((doc) => {
       return {
         id: doc.id,
         ...doc.data(),
