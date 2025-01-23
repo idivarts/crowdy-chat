d25e7cfac638d1a570e9188afb8c51709333afe3
Rahul Sinha <rahul@idiv.in>
2025-01-23 13:22:54 +0530

    Done the changes for fixing cc

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,18 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase-config";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.CROWDY_CHAT_API_KEY,
-  authDomain: process.env.CROWDY_CHAT_AUTH_DOMAIN,
-  projectId: process.env.CROWDY_CHAT_PROJECT_ID,
-  storageBucket: process.env.CROWDY_CHAT_STORAGE_BUCKET,
-  messagingSenderId: process.env.CROWDY_CHAT_MESSAGING_SENDER_ID,
-  appId: process.env.CROWDY_CHAT_APP_ID,
-  measurementId: process.env.CROWDY_CHAT_MEASUREMENT_ID,
-};
-console.log("firebaseConfig", firebaseConfig);
+// console.log("firebaseConfig", firebaseConfig);
 
 
 // Initialize Firebase
